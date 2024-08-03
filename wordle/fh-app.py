6546777a2b73f5c6879d3c55f783dbe6d375504d
Neeth <neeth.xavier@gmail.com>
2024-08-03 18:21:05 -0400

    wip fh-app

---
+++ wordle/fh-app.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+from wordle.wordle import Game
+
+from fasthtml import common
+from fasthtml import live_reload, ft
+
+
+class GameFH(Game):
+    def __init__(self, word, vs_mode=False):
+        super().__init__(word=word, vs_mode=vs_mode)
+        self.cur_pos = 0
+
+    def get_next_guess(self) -> str:
+        pass
+
+
+game = GameFH("scams")
+
+css = common.StyleX((Path(__file__).parent / "assets/wordle.css").resolve())
+app = live_reload.FastHTMLWithLiveReload(hdrs=(css,))
+
+
+def play_area():
+    n_rows = 5
+    n_cols = 5
+    return ft.Div(
+        *[
+            ft.Div(*[ft.Div(f"{i}_{j}", id="box_{i}{j}", cls="cell") for j in range(n_cols)], id=f"row_{i}", cls="row")
+            for i in range(n_rows)
+        ],
+        cls="table",
+    )
+    # return ft.Table(
+    #    *[
+    #        ft.Tr(*[ft.Td(f"{i}_{j}", id="box_{i}{j}", cls="cell") for j in range(n_cols)], id=f"row_{i}", cls="row")
+    #        for i in range(n_rows)
+    #    ]
+    # )
+
+
+@app.get("/")
+def setup():
+    return ft.Div(ft.H1("Wordle"), play_area(), cls="contianer")
+
+
+common.serve()
